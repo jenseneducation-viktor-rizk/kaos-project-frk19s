bfaf90e779aaa40ec9b0c087e447ba5bc48aedbd
madokafujimakiahlsen <madoka.fujimakiahlsen@gmail.com>
2020-06-08 14:28:54 +0200

    added base of admin page

--- src/components/admin/Form.vue
+++ src/components/admin/Form.vue
@@ -1,8 +1,7 @@
-<template></template>
+<template> <div></div></template>
 
 <script>
 export default {};
 </script>
 
-<style>
-</style>
+<style></style>
---
+++ src/views/admin/Admin.vue
@@ -0,0 +1,27 @@
+<template>
+  <div id="admin">
+    <Events />
+    <Form />
+  </div>
+</template>
+
+<script>
+import Events from "@/components/admin/Events.vue";
+import Form from "@/components/admin/Form.vue";
+
+export default {
+  name: "Admin",
+  components: {
+    Events,
+    Form,
+  },
+};
+</script>
+
+<style lang="scss">
+@import "@/scss/_variables.scss";
+
+#admin {
+  background-color: $purple;
+}
+</style>
