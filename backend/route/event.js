9a256b42e850f22061d160340247160af733ac10
Adetokunbo Adenuga <adetokunboadenuga@gmail.com>
2020-06-08 14:14:43 +0200

    Link app.js to event.js

--- backend/route/event.js
+++ backend/route/event.js
@@ -1,13 +1,14 @@
 const { Router } = require("express");
 const router = new Router();
 const fs = require("fs");
-const low = require("lowdb");
-const FileSync = require("lowdb/adapters/FileSync");
-const adapter = new FileSync("db.json");
-const db = low(adapter);
+// const low = require("lowdb");
+// const FileSync = require("lowdb/adapters/FileSync");
+// const adapter = new FileSync("db.json");
+// const db = low(adapter);
 
 router.get("/", async (req, res) => {
     const events = fs.createReadStream("db.json");
     events.pipe(res)
 });
 
+module.exports = router; 
