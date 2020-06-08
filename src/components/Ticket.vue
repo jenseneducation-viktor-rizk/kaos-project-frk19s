38270513d09d69df5fa5908f6c8ef2046bdce8f6
jenseneducation-viktor-rizk <55272941+jenseneducation-viktor-rizk@users.noreply.github.com>
2020-06-08 14:05:02 +0100

    tickets started

---
+++ src/components/Ticket.vue
@@ -0,0 +1,21 @@
+<template>
+  <div id="ticket">
+      
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+
+</script>
+
+<style lang="scss" scoped>
+    #ticket {
+        width: 70%;
+        height: 70%;
+        background-color: rgba($color: #f8f8f8, $alpha: 1.0);
+        box-shadow: 0px 0px 6px 3px;
+    }
+</style>
---
+++ src/views/Tickets.vue
@@ -0,0 +1,30 @@
+<template>
+  <div id="tickets">
+      <Ticket />
+  </div>
+</template>
+
+<script>
+import Ticket from '../components/Ticket'
+
+
+export default {
+    components: { Ticket }
+
+}
+</script>
+
+<style lang="scss" scoped>
+    #tickets {
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        margin: 10px;
+        padding: 0;
+        max-height: 667px;
+        max-width: 475px;
+        height: 100vh;
+        width: 100vw;
+        background-color: rgba($color: #542bc7, $alpha: 1.0);
+    }
+</style>
