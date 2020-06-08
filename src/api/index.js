11befdfb776f659e8cd2caa74daeff12e046c41e
Artur <Artur.Hoppner@hotmail.com>
2020-06-08 13:53:25 +0200

    added obejcts to post

--- src/api/index.js
+++ src/api/index.js
@@ -5,7 +5,43 @@ const api = axios.create({
 });
 
 async function fetchEvents() {
-    const res = await api.get("/event");
-    const data = res.data.events
-    return data;
+    try {
+        const res = await api.get("/");
+        const data = res.data.events
+        return data;
+    } catch (error) {
+        console.log(error, 'error in fetching Event')
+      }
 }
+
+async function addNewEvent() {
+    try {
+        const res = await api.post('/',
+            {     
+                name: empty, 
+                where: empty, 
+                date: empty, 
+                timefrom: empty, 
+                timeTo: empty, 
+                Price: empty
+            })
+        const data = res.data.events;
+        return data
+    } catch (error) {
+        console.log(error, 'error in posting new event')
+      }
+}
+
+async function boughtTicket() {
+    try {
+        const res = await api.post('/incrementTicket',
+            { increment: ticket })
+
+        const data = res.data.events;
+        return data
+    } catch (error) {
+        console.log(error, 'error in post bought ticket')
+      }
+}
+
+export {fetchEvents, addNewEvent, boughtTicket}
