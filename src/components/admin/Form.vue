0d14f58072f9306c1e249b2d471c67bdc6339594
jensen-education-razieh-rouzbahani <razie.rouzbahani@gmail.com>
2020-06-10 16:33:31 +0200

    Form added
    Form added

--- src/components/admin/Form.vue
+++ src/components/admin/Form.vue
@@ -4,16 +4,51 @@
       <h2>Add Event</h2>
 
       <div class="inputWide">
-        <input type="text" class="wide" placeholder="Name" v-model="newEvent.name" />
-        <input type="text" class="wide" placeholder="Where"  v-model="newEvent.place"/>
-        <input type="text" class="wide" placeholder="Date" v-model="newEvent.date"/>
+        <input
+          type="text"
+          class="wide"
+          placeholder="Name"
+          v-model="newEvent.name"
+        />
+        <input
+          type="text"
+          class="wide"
+          placeholder="Where"
+          v-model="newEvent.place"
+        />
+        <input
+          type="text"
+          class="wide"
+          placeholder="Date"
+          v-model="newEvent.date"
+        />
       </div>
 
       <div class="inputQuad">
-        <input type="text" class="half" placeholder="From Time" v-model="newEvent.startTime"/>
-        <input type="text" class="half" placeholder="To Time" v-model="newEvent.endTime"/>
-        <input type="number" class="half" placeholder="Tickets" v-model="newEvent.tickets.spots"/>
-        <input type="number" class="half" placeholder="Price" v-model="newEvent.price"/>
+        <input
+          type="text"
+          class="half"
+          placeholder="From Time"
+          v-model="newEvent.startTime"
+        />
+        <input
+          type="text"
+          class="half"
+          placeholder="To Time"
+          v-model="newEvent.endTime"
+        />
+        <input
+          type="number"
+          class="half"
+          placeholder="Tickets"
+          v-model="newEvent.tickets.spots"
+        />
+        <input
+          type="number"
+          class="half"
+          placeholder="Price"
+          v-model="newEvent.price"
+        />
       </div>
 
       <button>Add the Event</button>
@@ -22,7 +57,7 @@
 </template>
 <script>
 export default {
-  name: 'Form',
+  name: "Form",
   data() {
     return {
       newEvent: {
@@ -38,26 +73,26 @@ export default {
         endTime: "",
         tickets: {
           spots: Number,
-          sold: 0
-        }
-      }
-    }
+          sold: 0,
+        },
+      },
+    };
   },
   computed: {
     events() {
-      return this.$store.state.events
-    }
+      return this.$store.state.events;
+    },
   },
   methods: {
     async createEvent() {
-      this.$store.dispatch('createEvent', this.newEvent);
-      this.$store.dispatch('getEvents')
-    }
+      this.$store.dispatch("createEvent", this.newEvent);
+      this.$store.dispatch("getEvents");
+    },
   },
   beforeMount() {
-    this.$store.dispatch('getEvents');
-  }
-}
+    this.$store.dispatch("getEvents");
+  },
+};
 </script>
 
 <style lang="scss">
@@ -70,29 +105,27 @@ export default {
   align-items: center;
   background: $purple;
   height: 100vh;
-  
+
   .form {
     display: flex;
     flex-direction: column;
     align-items: flex-start;
     padding: 20px 50px;
-    
+
     width: 432px;
     background: $gray;
-    h2{
+    h2 {
       color: $pink;
       font-family: Fira Sans;
       font-style: normal;
       font-weight: bold;
-      
     }
-    .inputWide{
+    .inputWide {
       display: flex;
       flex-direction: column;
       width: 100%;
-      
-      
-      .wide{
+
+      .wide {
         height: 40px;
         background: transparent;
         margin-bottom: 12px;
@@ -103,14 +136,12 @@ export default {
         font-family: Fira Sans;
         font-style: normal;
         font-weight: 200;
-        
       }
     }
-    .inputQuad{
-     
+    .inputQuad {
       margin: 0 auto -13px;
-      
-      .half{
+
+      .half {
         height: 40px;
         width: calc(100% / 2 - 16px);
         background: transparent;
@@ -119,24 +150,23 @@ export default {
         border-radius: 3px;
         margin-right: 20px;
         margin-bottom: 13px;
-        &:nth-child(2n){
+        &:nth-child(2n) {
           margin-right: 0;
         }
         .half::placeholder {
-        font-family: Fira Sans;
-        font-style: normal;
-        font-weight: 200;
-        
-      }
+          font-family: Fira Sans;
+          font-style: normal;
+          font-weight: 200;
+        }
       }
     }
-    button{
-        height: 40px;
-        width: calc(100% / 2 - 16px);
-        background: $pink;
-        color: $white;
-        border: 1px solid rgba(255, 255, 255, 0.7);
-        border-radius: 3px;
+    button {
+      height: 40px;
+      width: calc(100% / 2 - 16px);
+      background: $pink;
+      color: $white;
+      border: 1px solid rgba(255, 255, 255, 0.7);
+      border-radius: 3px;
     }
   }
 }
